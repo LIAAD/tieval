33c4c25cb74dc948181a84a44d8095fe34f220d2
hugo.o.sousa <hugo.o.sousa@inesctec.pt>
2021-09-21 15:29:36 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,39 @@
+import io
+import os
+import re
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
-setup(name="text2timeline", packages=find_packages())
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="text2timeline",
+    version="0.1.0",
+    url="https://github.com/hmosousa/text2timeline",
+    license='MIT',
+
+    author="Hugo Sousa",
+    author_email="hugo.o.sousa@inesctec.pt",
+
+    description=" ",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
