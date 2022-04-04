f128f8be81c48e4cd869fae6dc96a8fbb32b8aec
Hugo Sousa <hugo.o.sousa@inesctec.pt>
2022-04-04 15:29:09 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ README = open("README.md").read()
 setup(
     name="tieval",
     version="0.0.2",
-    url="https://github.com/hmosousa/text2timeline",
+    url="https://github.com/LIAAD/tieval",
     license='MIT',
 
     author="Hugo Sousa",
     author_email="hugo.o.sousa@inesctec.pt",
 
     description=
-    "This framework facilitates the development of temporal aware models",
+    "This framework facilitates the development and test of temporal aware models",
 
     long_description_content_type='text/markdown',
 
@@ -32,6 +32,17 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
-    python_requires=">=3.8"
+
+    python_requires=">=3.8",
+
+    extras_requires={
+        "dev": [
+            "py_test>=3.7",
+            "check-manifest",
+            "twine"
+        ]
+    },
+
 )
