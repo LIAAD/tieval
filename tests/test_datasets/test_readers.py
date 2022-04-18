b8346f53a63af75edeca9ec4a0d935e8e72cd8be
Hugo Sousa <hugo.o.sousa@inesctec.pt>
2022-04-18 12:31:29 +0100

    Fixed test_readers.py DATA_PATH

--- tests/test_datasets/test_readers.py
+++ tests/test_datasets/test_readers.py
@@ -1,70 +1,51 @@
 from pathlib import Path
 
-from tieval.datasets.readers import \
-    MeanTimeDocumentReader, \
-    TempEval3DocumentReader, \
-    GraphEveDocumentReader, \
-    TempEval2DocumentReader, \
-    TempEval2FrenchDocumentReader, \
-    TimeBank12DocumentReader, \
-    TCRDocumentReader, \
+from tieval.datasets.readers import (
+    MeanTimeDocumentReader,
+    TempEval3DocumentReader,
+    GraphEveDocumentReader,
+    TempEval2DocumentReader,
+    TempEval2FrenchDocumentReader,
+    TimeBank12DocumentReader,
+    TCRDocumentReader,
     TimeBankPTDocumentReader
+)
 
 from tieval.datasets.readers import JSONDatasetReader
 
-DATA_PATH = Path("../../data")
+DATA_PATH = Path(__file__).parent.parent / "files"
 
 
 class TestTempEval2DocumentReader:
-    reader = TempEval2DocumentReader(DATA_PATH / "tempeval_2_italian/train/cs.morph015.json")
+    reader = TempEval2DocumentReader(DATA_PATH / "tempeval_2_italian.json")
 
     def test_read(self):
         self.reader.read()
 
 
 class TestTimeBank12DocumentReader:
-    reader = TimeBank12DocumentReader(DATA_PATH / "timebank_1.2/train/wsj_0685.tml")
+    reader = TimeBank12DocumentReader(DATA_PATH / "timebank12.tml")
 
     def test_read(self):
         self.reader.read()
 
 
 class TestTCRDocumentReader:
-    reader = TCRDocumentReader(DATA_PATH / "tcr/train/2010.03.23.how.get.headhunted.tml")
+    reader = TCRDocumentReader(DATA_PATH / "tcr.tml")
 
     def test_read(self):
         self.reader.read()
 
 
 class TestTimeBankPTDocumentReader:
-    reader = TimeBankPTDocumentReader(DATA_PATH / "timebankpt/train/APW19980227.0476.tml")
+    reader = TimeBankPTDocumentReader(DATA_PATH / "timebankpt.tml")
 
     def test_read(self):
         self.reader.read()
 
 
 class TestTempEval2FrenchDocumentReader:
-    reader = TempEval2FrenchDocumentReader(DATA_PATH / "tempeval_2_french/train/baldwin_frratrain_28.xml")
+    reader = TempEval2FrenchDocumentReader(DATA_PATH / "tempeval_2_french.xml")
 
     def test_read(self):
         self.reader.read()
-
-
-class TestJSONDatasetReader:
-
-    def test_read(self):
-
-        reader = JSONDatasetReader(TempEval2DocumentReader)
-        te2 = reader.read(DATA_PATH / "tempeval_2_chinese")
-
-        reader = JSONDatasetReader(TempEval2DocumentReader)
-        te2 = reader.read(DATA_PATH / "tempeval_2_english")
-
-        reader = JSONDatasetReader(TempEval2DocumentReader)
-        te2 = reader.read(DATA_PATH / "tempeval_2_italian")
-
-        reader = JSONDatasetReader(TempEval2DocumentReader)
-        te2 = reader.read(DATA_PATH / "tempeval_2_korean")
-
-        reader = JSONDatasetReader(TempEval2DocumentReader)
-        te2 = reader.read(DATA_PATH / "tempeval_2_spanish")
