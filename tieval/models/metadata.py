e29c198abd4ae622caa28d93672657ee74a0abf2
Hugo Sousa <hugo.o.sousa@inesctec.pt>
2022-05-12 12:01:59 +0100

    Added CogCompTime2 metadata

--- tieval/models/metadata.py
+++ tieval/models/metadata.py
@@ -1,5 +1,7 @@
 
 MODELS_URL = {
     "event_identification": "https://drive.inesctec.pt/s/PbPeLPibQwFetBz/download",
-    "timex_identification": "https://drive.inesctec.pt/s/63dqAKAPkRLLBmf/download"
+    "timex_identification": "https://drive.inesctec.pt/s/63dqAKAPkRLLBmf/download",
+    "cogcomp2": "https://drive.inesctec.pt/s/Pq8H3ZJzB2wXs6f/download",
+    "cogcomp2_embeddings": "https://drive.inesctec.pt/s/CkGZ8xGSGaGKw2f/download"
 }
