e37f85fbedc8fb871dd7f99d9f0d58d58511c213
Hugo Sousa <hugo.o.sousa@inesctec.pt>
2022-05-20 10:23:02 +0100

    Added test_closure over all datasets.

--- tests/test_datasets/test_validate_data.py
+++ tests/test_datasets/test_validate_data.py
@@ -1,10 +1,8 @@
-import logging
+import collections
 import os
 
 from tieval import datasets
 from tieval.entities import Timex
-
-logging.basicConfig(level=logging.DEBUG)
 
 
 def test_endpoints(tmp_path):
@@ -27,3 +25,39 @@ def test_endpoints(tmp_path):
                 s, e = ent.endpoints
                 msg = f"Dataset: {dataset}\nDocument: {doc.name}\nExpected:_{ent.text}_\nObserved:_{doc.text[s:e]}_"
                 assert doc.text[s:e] == ent.text, msg
+
+
+def test_closure(tmp_path):
+
+    os.chdir(tmp_path)
+
+    n_datasets = len(datasets.SUPPORTED_DATASETS)
+    for dataset_id, dataset in enumerate(datasets.SUPPORTED_DATASETS):
+        print(f"Working in dataset {dataset} {dataset_id}/{n_datasets}")
+
+        data = datasets.read(dataset)
+        n_docs = len(data)
+        for doc_id, doc in enumerate(data.documents):
+            print(f"Working in document {doc.name} {doc_id}/{n_docs}")
+
+            tls = doc.tlinks
+            tls_inf = doc.temporal_closure
+
+            msg = f"Number o original tlinks is smaller than the one inferred for document {doc.name} in dataset {dataset}"
+            assert len(tls) <= len(tls_inf), msg
+
+            entity_pairs = collections.defaultdict(list)
+            for tl in tls_inf:
+                # sorted is to ensure that the assertation is independent of the source and target order.
+                key = tuple(sorted([tl.source.id, tl.target.id]))
+                entity_pairs[key] += [tl]
+
+            for key, value in entity_pairs.items():
+                msg = f"Inferred tlinks contains more than one relation for the same entity pair in {doc.name} in dataset {dataset}"
+                if len(value) > 1:
+                    for v in value:
+                        print(v.id)
+                    print()
+
+
+
