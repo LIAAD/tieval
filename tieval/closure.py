2986f7000b26693a595afa246908030c76642c0e
Hugo Sousa <hugo.o.sousa@inesctec.pt>
2022-05-21 10:56:06 +0100

    Fixed issue when computing closure

--- tieval/closure.py
+++ tieval/closure.py
@@ -1,6 +1,6 @@
+from collections import defaultdict
 import copy
 from typing import Set
-from collections import defaultdict
 
 from tieval.links import TLink
 
@@ -33,9 +33,9 @@ def temporal_closure(
 
     # build a dictionary of entities and their tlinks
     entities_tlinks = defaultdict(set)
-    for tl in tlinks:
-        entities_tlinks[tl.source].add(tl)
-        entities_tlinks[tl.target].add(tl)
+    for tlink in tlinks:
+        entities_tlinks[tlink.source].add(tlink)
+        entities_tlinks[tlink.target].add(tlink)
 
     old_tlinks = tlinks
     while True:
@@ -47,29 +47,27 @@ def temporal_closure(
                 entities_tlinks[tlink1.source],
                 entities_tlinks[tlink1.target]
             )
-
             connected_tlinks.discard(tlink1)
 
             for tlink2 in connected_tlinks:
 
                 inferred_tlink = tlink1 & tlink2
 
+                if inferred_tlink is not None:
+                    continue
+
                 # check if the inferred tlink is novel
-                already_inferred = (inferred_tlink in result)
-                cond = inferred_tlink and not already_inferred
+                cond = (inferred_tlink in result)
                 if not allow_incomplete:
                     cond = cond and inferred_tlink.relation.is_complete()
 
                 if cond:
 
-                    if inferred_tlink.source == "L" and inferred_tlink.target == "K":
-                        print()
-
                     inferred_tlinks.add(inferred_tlink)
 
                     # update entities_tlinks dict
                     entities_tlinks[inferred_tlink.source].add(inferred_tlink)
-                    entities_tlinks[inferred_tlink.source].add(inferred_tlink)
+                    entities_tlinks[inferred_tlink.target].add(inferred_tlink)
 
         if inferred_tlinks:
             result.update(inferred_tlinks)
@@ -77,5 +75,40 @@ def temporal_closure(
 
         else:
             break
+
+    # remove inferred tlinks that are an incomplete inference of existing ones.
+    # example: A --BEFORE--> B and A --BEFORE-OR-OVERLAP--> B
+    to_remove = []
+    entity_pairs = {}
+    for tlink in result:
+        key = tuple(sorted([tlink.source.id, tlink.target.id]))
+
+        if key not in entity_pairs:
+            entity_pairs[key] = tlink
+
+        else:
+            prev_tlink = entity_pairs[key]
+
+            if tlink.source != prev_tlink.source:
+                prev_tlink = ~prev_tlink
+
+            print(f"{tlink}\n{prev_tlink}")
+
+            # check if one point relation is contained in the other
+            # example: point relation [<, <, None, <] is contained in [<, <, <, <]
+            # an example that would not be removed: ['<', '<', '>', '>'] and ['>', '>', '>', '>']
+            # we don't what this links to be removed as they expose errors in the annotation
+            is_contained = True
+            for p1, p2 in zip(prev_tlink.relation.point, tlink.relation.point):
+
+                if p1 is None or p2 is None:
+                    continue
+
+                elif p1 != p2:
+                    is_contained = False
+                    break
+
+            if is_contained:
+                to_remove += [tlink]
 
     return result
