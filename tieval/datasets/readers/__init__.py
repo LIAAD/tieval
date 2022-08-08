11ae388e51162108b0047bc3d13908a68dbc9750
hugosousa <hmosousa@gmail.com>
2022-08-08 15:50:32 +0100

    Code simplification.

--- tieval/datasets/readers/__init__.py
+++ tieval/datasets/readers/__init__.py
@@ -1,22 +1,2 @@
-from tieval.datasets.readers.document import (
-    MeanTimeDocumentReader,
-    TimeBank12DocumentReader,
-    TCRDocumentReader,
-    TimeBankPTDocumentReader,
-    GraphEveDocumentReader,
-    TempEval3DocumentReader,
-    TempEval2FrenchDocumentReader,
-    TempEval2DocumentReader,
-    KRAUTSDocumentReader,
-    NarrativeContainerDocumentReader,
-    DocumentReaders
-)
-from tieval.datasets.readers.dataset import (
-    XMLDatasetReader,
-    TDDiscourseDatasetReader,
-    EventTimeDatasetReader,
-    TimeBankDenseDatasetReader,
-    MATRESDatasetReader,
-    JSONDatasetReader,
-    DatasetReaders
-)
+from tieval.datasets.readers.document import *
+from tieval.datasets.readers.dataset import *
