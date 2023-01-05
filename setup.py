029aadb5121741a56927b14334b2f64ecd40ae58
Hugo Sousa <hmosousa@gmail.com>
2023-01-05 18:59:21 +0000

    Unattached py_heideltime from a specific version.

--- setup.py
+++ setup.py
@@ -6,22 +6,16 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="tieval",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/LIAAD/tieval",
     license='MIT',
-
     author="Hugo Sousa",
     author_email="hugo.o.sousa@inesctec.pt",
-
     description=
     "This framework facilitates the development and test of temporal-aware models.",
-
     long_description_content_type='text/markdown',
-
     long_description=README,
-
     packages=find_packages(exclude=('tests*',)),
-
     install_requires=[
         "allennlp==2.9.3",
         "nltk==3.6.7",
@@ -29,7 +23,7 @@ setup(
         "torch==1.11.0",
         "xmltodict==0.12.0",
         "networkx==2.8.1",
-        "py-heideltime @ git+https://github.com/JMendes1995/py_heideltime.git@fc5a81692e73e0c6ed59bf002d1c0afa506f2d39",
+        "py-heideltime @ git+https://github.com/JMendes1995/py_heideltime.git",
         "cached-path==1.1.2"
     ],
 
@@ -41,9 +35,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
-
     python_requires=">=3.8",
-
     extras_requires={
         "dev": [
             "py_test>=3.7",
