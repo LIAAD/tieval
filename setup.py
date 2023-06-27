0a5a57cf80aeb9836a7c53f68f477d2e4c477674
Hugo Sousa <hmosousa@gmail.com>
2023-06-27 16:51:20 +0100

    Increased patch in version to update on PyPI. Closes #15

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="tieval",
-    version="0.0.4",
+    version="0.0.6",
     url="https://github.com/LIAAD/tieval",
     license='MIT',
     author="Hugo Sousa",
