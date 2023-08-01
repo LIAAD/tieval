634fc0b78bee4532f252581ce75d4c1189c0431c
Hugo Sousa <hugo.o.sousa@inesctec.pt>
2023-08-01 11:48:34 +0100

    Fixed CogComp broken links. Closes #16

--- tieval/models/metadata.py
+++ tieval/models/metadata.py
@@ -1,6 +1,6 @@
 MODELS_URL = {
     "event_identification": "https://drive.inesctec.pt/s/PbPeLPibQwFetBz/download",
     "timex_identification": "https://drive.inesctec.pt/s/63dqAKAPkRLLBmf/download",
-    "cogcomp2": "https://drive.inesctec.pt/s/Pq8H3ZJzB2wXs6f/download",
-    "cogcomp2_vocab": "https://drive.inesctec.pt/s/KyYcd92q4sYjnm2/download"
+    "cogcomp2": "https://drive.inesctec.pt/s/ZZ7JcDm8RxNkNPC/download",
+    "cogcomp2_vocab": "https://drive.inesctec.pt/s/pEAgToMMFTerSsL/download"
 }
