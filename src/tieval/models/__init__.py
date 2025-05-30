43dbfc24641be0284343ebbb7d114218ef6b55ad
Hugo Sousa <hugo.o.sousa@inesctec.pt>
2025-05-30 17:33:10 +0100

    Remove CogComp2

--- src/tieval/models/__init__.py
+++ src/tieval/models/__init__.py
@@ -2,7 +2,6 @@ from tieval.models import classification
 from tieval.models import identification
 from tieval.models.base import BaseModel
 from tieval.models.base import BaseTrainableModel
-from tieval.models.classification import CogCompTime2
 from tieval.models.identification import EventIdentificationBaseline
 from tieval.models.identification import HeidelTime
 from tieval.models.identification import TimexIdentificationBaseline
